4875b6927627a1fcb6895e483bd3610278ece87b
maulvialf <maulvialf@gmail.com>
2023-04-27 07:21:13 +0700

    fix ws

--- 2023/angstrom-2023/rev/wordsearch/solve.py
+++ 2023/angstrom-2023/rev/wordsearch/solve.py
@@ -1,3 +1,12 @@
+import itertools
+import nltk
+
+# Download the words dataset from nltk
+nltk.download('words')
+
+# Define the dataset
+
+
 dataset = """hbjU5
 ofM
 tCl
@@ -34,3 +43,21 @@ dataset = dataset.split("\n\n")
 dataset = [x.split("\n") for x in dataset]
 from pprint import pprint as pp
 pp(dataset)
+dataset = dataset[3]
+
+# both irregular and inexpressive
+# Get English words list
+english_words = set(nltk.corpus.words.words())
+
+# Get all possible character combinations
+combinations = list(itertools.product(*dataset))
+
+# Check each combination to see if it forms a valid English word
+valid_words = []
+for combination in combinations:
+    word = ''.join(combination)
+    if word.lower() in english_words:
+        valid_words.append(word)
+
+# Print the valid English words
+print("Valid English words found:", valid_words)
