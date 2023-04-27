57bad6c9de2bb6b78118c3094d50b7599217bf6f
maulvialf <maulvialf@gmail.com>
2023-04-27 07:20:20 +0700

    add simon

---
+++ 2023/angstrom-2023/misc/simon/solve.py
@@ -0,0 +1,27 @@
+from pwn import *
+
+p = connect("challs.actf.co", 31402)
+
+j = 0
+while True:
+    try:
+        print(j)
+        data = p.recvline()
+        print(data)
+        data = data.split(b"letters of ")
+        word = []
+        for i in range(1, len(data)):
+            fw = data[i].split(b" ")[0]
+            fw = fw.strip()
+            word.append(fw)
+        from pprint import pprint as pp
+        # print(word)
+        hasil = word[0][:3] + word[1][-3:]
+        print(hasil, j)
+        p.sendline(hasil)
+        j += 1
+    except Exception as e:
+        print(e)
+        break
+
+p.interactive()
